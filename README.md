3cd0ad2f05172977d1419cd284831b5024c56e84
Dmitry Mishin <dmitry@pha.jhu.edu>
2013-04-30 17:41:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-vobox-python-client
-===================
+VOBox Python Client
+==================
 
-Python command-line client for VOBox
+A Python module that provides the tools you need to authenticate with, and use the VOBox's Dropbox API.
+
+Getting started 
+---------------
+To set up the VOBox module and install dependencies:
+
+1.  Run the setup script:
+        $ python setup.py install
+2.  Try out the VOBox "shell" by running the vobox client. Run: 
+        $ vobox info
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import subprocess
+import shutil
+import sys
+
+from setuptools import setup, find_packages
+
+parent_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+INSTALL_REQUIRES = ['config']
+if sys.version_info < (2, 6):
+    INSTALL_REQUIRES.append('simplejson') # The 'json' module is included with Python 2.6+
+
+setup(name='vobox-python-client',
+      version='0.1.0',
+      description='VOBox REST API Client',
+      author='Dmitry Mishin',
+      author_email='dmitry@pha.jhu.edu',
+      url='http://usvao.org',
+      scripts=['bin/vobox'],
+      packages=['dropbox', 'tests'],
+      install_requires=INSTALL_REQUIRES,
+      long_description=open('README.md').read(),
+     )
