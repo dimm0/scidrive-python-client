ccaf6c48bff08110b4de5f5aca392caa99170165
Dmitry Mishin <dmitry@pha.jhu.edu>
2013-05-01 12:39:12 -0400

    Dependencies updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 parent_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-INSTALL_REQUIRES = ['config']
+INSTALL_REQUIRES = ['config', 'argparse']
 if sys.version_info < (2, 6):
     INSTALL_REQUIRES.append('simplejson') # The 'json' module is included with Python 2.6+
 
